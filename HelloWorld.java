a6dd44ad2237bd197fe888cfdd98a3efc8165031
Win <1057314504@qq.com>
2020-03-31 09:13:25 +0800

    first program

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+//公开的 类 类名
+public class HelloWorld//一个叫HellolWorld的类
+{	static int a=0;
+	//类体
+	//除了声明变量不能在这写东西
+	//下面是主方法
+	public static void main(String[] args){//公开 静态 空 方法名   args是变量名
+		//方法体
+	System.out.println("Hello Java!");//向控制台输出消息
+	//不能放到类体
+	char c = '\u9999';
+	System.out.println(c);
+	}
+}
